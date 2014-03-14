f0e9a612d673e66586ec84aabd554d34a9597acd
zrathustra <ahr12012@mymail.pomona.edu>
2014-03-14 12:01:10 -0700

    FIRST

---
+++ linear_lattice.py
@@ -0,0 +1,13 @@
+import random as r
+
+class LinearLattice(VectorSpace):
+
+    def __init__(self, dim, f):
+        return VectorSpace(GF(f),dim)
+
+    def random_family(self, k, size):
+        subspaces = list(self.subspaces(k))
+
+        xs = [for i in range(size): r.randint(0,len(subspaces))]
+
+        return [for x in xs: subspaces[x]]
