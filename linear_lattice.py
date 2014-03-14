a5b96a4639b8158eb1f9449a0ead3328cf5538b3
Gabe <gabrielecurrier@gmail.com>
2014-03-14 12:42:41 -0700

    3 clusters!

--- linear_lattice.py
+++ linear_lattice.py
@@ -11,3 +11,11 @@ class LinearLattice(VectorSpace):
         xs = [for i in range(size): r.randint(0,len(subspaces))]
 
         return [for x in xs: subspaces[x]]
+
+    def is3clust(self,fam):
+    	k = fam[0].dimension()
+    	intersect = fam[0].intersection(fam[1].intersection(fam[2]))
+    	vsum = sum(fam)
+
+    	return intersection.dimension() == 0 and vsum.dimension() <= 2*k
+
