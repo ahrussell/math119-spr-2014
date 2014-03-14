24d9408756b94cb703a3f7acb66b2c1c44ba2a3b
zrathustra <ahr12012@mymail.pomona.edu>
2014-03-14 13:55:38 -0700

    asdf

--- linear_lattice.py
+++ linear_lattice.py
@@ -1,21 +1,55 @@
 import random as r
 
-class LinearLattice(VectorSpace):
+def LinearLattice(f, dim):
+    return VectorSpace(GF(f),dim)
 
-    def __init__(self, f, dim):
-        return VectorSpace(GF(f),dim)
+def random_family(subspaces, size):
+    xs = []
 
-    def random_family(self, k, size):
-        subspaces = list(self.subspaces(k))
+    while len(xs) < size:
+        x = r.randint(0,len(subspaces)-1)
+       
+        if x not in xs:
+            xs.append(x)
 
-        xs = [for i in range(size): r.randint(0,len(subspaces))]
+    return [subspaces[x] for x in xs]
 
-        return [for x in xs: subspaces[x]]
+def is3clust(fam):
+    k = fam[0].dimension()
+    intersect = fam[0].intersection(fam[1].intersection(fam[2]))
+    vsum = sum(fam)
 
-    def is3clust(self,fam):
-    	k = fam[0].dimension()
-    	intersect = fam[0].intersection(fam[1].intersection(fam[2]))
-    	vsum = sum(fam)
+    return intersect.dimension() == 0 and vsum.dimension() <= 2*k
 
-    	return intersection.dimension() == 0 and vsum.dimension() <= 2*k
+def detect_3_cluster(fam):
+    m = len(fam)
+
+    for i in range(m):
+        for j in range(i+1,m):
+            for k in range(j+1,m):
+                if is3clust([fam[i],fam[j],fam[k]]):
+                    return True, [fam[i],fam[j],fam[k]]
+    
+    return False, fam
+
+
+#######################
+# code for computing results
+#######################
+
+# L = LinearLattice(2,6)
+# S = list(L.subspaces(3))
+# 
+# results = []
+# 
+# for i in range(50):
+#     F = random_family(S,5)
+#     results.append(detect_3_cluster(F))
+# 
+# count = 0
+# for i in results:
+#     if i[0] == True:
+#         count += 1
+# 
+# print 100 * float(count) / len(results), "%"
 
